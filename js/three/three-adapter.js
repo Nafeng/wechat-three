de1edf614af6ff372c0e521065abb23148e16e51
Nafeng <nanaifeng@gmail.com>
2019-01-27 16:59:35 +0800

    fix error for fuc createElementNS

--- js/three/three-adapter.js
+++ js/three/three-adapter.js
@@ -1,3 +1,9 @@
 import "../libs/weapp-adapter.js"
+if (!document.createElementNS) {
+  delete document.createElementNS
+  document.createElementNS = function (ns, tagName) {
+    return document.createElement(tagName)
+  }
+}
 GameGlobal.THREE = require('./three.js')
 GameGlobal.OrbitControls = require( "./controls/OrbitControls.js")
